6181859ab20f4fe7d56863ffe3e655a85de37efc
Sanshiroh SAKAI <sansyo@gmail.com>
2025-05-02 15:44:11 +0900

    Update: Biome を導入

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google"
+import Image from "next/image"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export default function Home() {
   return (
@@ -111,5 +111,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
